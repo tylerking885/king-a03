a6374dbcea8bce662cc1b8b49b9c0e1d95797642
Tyler <tylerking885@knights.ucf.edu>
2021-09-23 14:02:41 -0400

    First commit with pseudocode.

---
+++ exercise26/src/main/java/baseline/Solution26.java
@@ -0,0 +1,23 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solution26
+ *  Copyright 2021 Tyler King
+ */
+
+package baseline;
+
+public class Solution26 {
+    // In function readDoubleFromUser:
+    // User will be prompted to enter their apr as a percentage.
+    // User will be prompted to enter their balance of credit card.
+    // User will be prompted to enter their monthly payment.
+
+    // In main:
+    // Prints the number of months based on the return of calcMonthsUntilPaidOff.
+}
+
+ class PaymentCalculator{
+     // In function int calculateMonthsUntilPaidOff:
+     // calculates the number of months based on the formula:
+     // n = -(1/30) * log(1 + b/p * (1 - (1 + i)^30)) / log(1 + i)
+     // returns the number of months left to pay off.
+}
