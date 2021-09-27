ed31c632427bdaf040ceaecdfd532f6667a006e8
Tyler <tylerking885@knights.ucf.edu>
2021-09-27 19:46:49 -0400

    added code to exercise25

--- exercise25/src/main/java/baseline/Solution25.java
+++ exercise25/src/main/java/baseline/Solution25.java
@@ -10,32 +10,79 @@ import java.util.Scanner;
 public class Solution25 {
     private static final Scanner in = new Scanner(System.in);
 
-    private char[] str1, str2;
+    private String str1;
 
     // In function readStringFromUser:
-    private String readStringFromUser(String prompt) {
+    private String readStringFromUser() {
 
         // Contains a prompt for user to enter a password.
-        System.out.print(prompt);
+        System.out.print("Enter a password: ");
 
         // Reads in a string from the user.
         return in.nextLine();
     }
 
     // In function public int passwordValidator:
-    // The function is used to determine the strength of the entered password from user.
-    // The function takes in the string entered by the user as a parameter.
-    // The function will initially take a string length and if the length is
-    // less than or equal to 5 then add 1 to a strength counter.
-    // Next check for numbers in the string. If the string contains numbers add 1 again to strength counter.
-    // Finally, check for special characters in the string. If this comes back true then add 1 more to strength counter.
+    public int passwordValidator(String str1) {
+    /*  Separate approach:
+        char ch;
+        boolean capitalFlag = false;
+        boolean lowerCaseFlag = false;
+        boolean numberFlag = false;
+        for(int i = 0; i < str1.length(); i++) {
+                ch = str1.charAt(i);
+                if( Character.isDigit(ch)) {
+                    numberFlag = true;
+                }
+                else if (Character.isUpperCase(ch)) {
+                    capitalFlag = true;
+                } else if (Character.isLowerCase(ch)) {
+                    lowerCaseFlag = true;
+                }
+                if(numberFlag && capitalFlag && lowerCaseFlag)
+                    return true;
+            }
+            return false;
+        } */
+    int strength = 0;
+
+    // If string length is less than 8 and contains only numbers, strength is 1.
+    if (str1.length() < 8 && str1.matches("[0-9]+"))
+        return strength + 1;
+    // If string length is less than 8 and contains only letters, strength is 2.
+    else if (str1.length() < 8 && str1.matches("[a-zA-Z]+"))
+        return strength + 2;
+    // If string length is at least 8, contains at least one number and contains letters, strength is 3.
+    else if (str1.length() >= 8 && str1.matches("[a-zA-Z]+") && str1.matches("[0-9]+"))
+        return strength + 3;
+    // If string length is at least 8, contains numbers, contains letters and special characters, strength is 4.
+    else if (str1.length() >= 8 && str1.matches("[a-zA-Z]+") && str1.matches("[0-9]+") && str1.matches("[`~!@#$%^&*()_+=]+"))
+        return strength + 4;
+    return strength;
+    }
 
     // In main:
     public static void main(String[] args) {
         Solution25 app = new Solution25();
+
+        app.str1 = app.readStringFromUser();
+
+        // If function passwordValidator returned a 0
+        if (app.passwordValidator(app.str1) == 1)
+            System.out.printf("The password '%s' is a very weak password.", app.str1);
+
+        // If function passwordValidator returned a 1
+        if (app.passwordValidator(app.str1) == 2)
+            System.out.printf("The password '%s' is a weak password.", app.str1);
+
+        // If function passwordValidator returned a 2
+        if (app.passwordValidator(app.str1) == 3)
+            System.out.printf("The password '%s' is a strong password.", app.str1);
+
+        // If function passwordValidator returned a 3
+        if (app.passwordValidator(app.str1) == 4)
+            System.out.printf("The password '%s' is a very strong password.", app.str1);
     }
-    // If function passwordValidator returned a 0 then print the string entered by user with message "is a very weak password."
-    // If function passwordValidator returned a 1 then print the string entered by user with message "is a weak password."
-    // If function passwordValidator returned a 2 then print the string entered by user with message "is a strong password."
-    // If function passwordValidator returned a 3 then print the string entered by user with message "is a very weak password."
 }
+
+
