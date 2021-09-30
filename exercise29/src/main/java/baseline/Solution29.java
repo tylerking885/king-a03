2b032bd8f87c3dcfcbdafb254319426fef78557e
Tyler <tylerking885@knights.ucf.edu>
2021-09-30 16:09:32 -0400

    added code to exercise29

--- exercise29/src/main/java/baseline/Solution29.java
+++ exercise29/src/main/java/baseline/Solution29.java
@@ -1,29 +1,50 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solution29
+ *  Copyright 2021 Tyler King
+ */
+
 package baseline;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Solution29 {
-    /*
-
-    Trap for bad user input with the use of try and catch.
-
-    User will be stuck in a while loop until they enter the correct input.
-
-    The two exceptions that will be trapped are division by 0 and input mismatch.
-
-     */
 
     // Demonstrates throwing an exception when a division by 0 occurs.
-    public static int InvestmentCalc(int years, int r)
+    public static int InvestmentCalc(int r)
         throws ArithmeticException{
-        return years = 72 / r;
+
+        // Here is where the possible incorrect inputs will occur.
+        return 72 / r;
+
     }
 
     public static void main(String[] args){
         Scanner in = new Scanner(System.in);
         boolean continueLoop = true;
 
+
+        do{
+            try{
+                System.out.print("What is the rate of return? ");
+                int r = in.nextInt();
+
+                int years = InvestmentCalc(r);
+                System.out.printf("It will take %d years to double your initial investment.", years);
+
+                continueLoop = false;
+            }
+            // Handles a case where user enters anything other than integers.
+            catch (InputMismatchException inputMismatchException){
+                System.out.printf("Sorry. That's not a valid input.%n");
+
+                // This discards user input.
+                in.nextLine();
+            }
+            // Handles a case where user enters 0.
+            catch (ArithmeticException arithmeticException){
+                System.out.printf("Sorry. That's not a valid input.%n");
+            }
+        } while (continueLoop);
     }
-
 }
-
