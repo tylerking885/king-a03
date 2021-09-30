47a0f8b0cd0ae6853e55d10f78e0bfba29309df7
Tyler <tylerking885@knights.ucf.edu>
2021-09-30 00:26:04 -0400

    added pseudocode to exercise27

---
+++ exercise27/src/main/java/baseline/Solution27.java
@@ -0,0 +1,31 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solution27
+ *  Copyright 2021 Tyler King
+ */
+
+package baseline;
+
+public class Solution27 {
+
+    // Function() that returns a string which will prompt user for a first name.
+    // Reuse previous function for a last name.
+
+    // Function() that measure the length of the previous entered strings.
+    // If the length is not greater than 2 print an error message.
+    // If the last name isn't at least 1 an additional error message prints.
+
+    // Function() that returns a string which will prompt user for a zip code.
+
+    // Function() to search the previous string for any lower or uppercase characters.
+    // If characters are present an error message will print.
+
+    // Function() that returns a string which will prompt user for an employee id.
+
+    // Function() that will determine if previously entered string is in format AA-1234.
+    // If string doesn't follow specific format an error message will print.
+
+    // Function validateInput() that uses previous input and validation functions.
+
+    // Print the results.
+
+}
