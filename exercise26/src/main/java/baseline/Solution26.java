d0bca4c3bea8029dd3ce934e15d7c64d93d497ec
Tyler <tylerking885@knights.ucf.edu>
2021-09-29 23:30:00 -0400

    added more code to exercise26

--- exercise26/src/main/java/baseline/Solution26.java
+++ exercise26/src/main/java/baseline/Solution26.java
@@ -9,52 +9,42 @@ import java.util.Scanner;
 
 public class Solution26 {
  private static final Scanner in = new Scanner(System.in);
- public double i;
- public double b;
- public double p;
+
  // In function readIntFromUser:
- public int readIntFromUser(String prompt) {
-
-  // Contains a prompt for user to enter a password.
-  System.out.print(prompt);
-
-  // Reads in a string from the user.
-  return in.nextInt();
- }
-
  public double readDoubleFromUser(String prompt) {
 
-  // Contains a prompt for user to enter a password.
+  // Contains a prompt for user to enter a value.
   System.out.print(prompt);
 
   // Reads in a string from the user.
   return in.nextDouble();
  }
- // In main:
+
  public static void main(String[] args) {
-  Solution26 app = new Solution26();
+  PaymentCalculator object = new PaymentCalculator();
+
   // Prints the number of months based on the return of calcMonthsUntilPaidOff.
+  System.out.printf("It will take you %d months to pay off this card.", object.calculateMonthsUntilPaidOff());
 
  }
-
 }
- class PaymentCalculator extends Solution26 {
 
-  // In function int calculateMonthsUntilPaidOff:
+class PaymentCalculator extends Solution26 {
+
+  // Returns the number of months left to pay off.
   public int calculateMonthsUntilPaidOff() {
-   PaymentCalculator app = new PaymentCalculator();
+   PaymentCalculator pay = new PaymentCalculator();
 
    // User will be prompted to enter their balance of credit card.
-   app.b = app.readDoubleFromUser("What is your balance? ");
+   double b = pay.readDoubleFromUser("What is your balance? ");
 
    // User will be prompted to enter their apr as a percentage.
-   app.i = (app.readDoubleFromUser("What is the APR on the card (as a percent)? "))/100;
+   double i = ((pay.readDoubleFromUser("What is the APR on the card (as a percent)? ")) / 100) / 365;
 
    // User will be prompted to enter their monthly payment.
-   app.p = app.readDoubleFromUser("What is the monthly payment you can make? ");
+   double p = pay.readDoubleFromUser("What is the monthly payment you can make? ");
 
-   // Returns the number of months left to pay off.
-   return (int) (-(1 / 30) * Math.log(1 + app.b / app.p * (1 - Math.pow((1 + app.i), 30)))/ Math.log(1 + app.i));
+                 // Formula for determining number of months to pay credit card off.
+   return (int) Math.ceil(((Math.log(1 + (b / p) * (1 - Math.pow((1 + i), 30)))) / (Math.log(1 + i))) / -30);
   }
-
- }
+}
