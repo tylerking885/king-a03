14353638455202b44455526bd3d9dacf5d9b2f5f
Tyler <tylerking885@knights.ucf.edu>
2021-09-30 14:27:31 -0400

    added pseudocode to exercise28

---
+++ exercise28/src/main/java/baseline/Solution28.java
@@ -0,0 +1,11 @@
+package baseline;
+
+public class Solution28 {
+    // There will be a prompt function.
+    // The function will consist of for loop that loops 5 times.
+    // Each loop the user will be prompted for a number.
+
+    // Another function will calculate and return the sum of the 5 numbers.
+
+    // The total will be printed at the end in main.
+}
