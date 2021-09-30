fbe2828f5a98a245938d6d052511ebe857225313
Tyler <tylerking885@knights.ucf.edu>
2021-09-30 15:40:51 -0400

    added some pseudocode for exercise29

---
+++ exercise29/src/main/java/baseline/Solution29.java
@@ -0,0 +1,29 @@
+package baseline;
+
+import java.util.Scanner;
+
+public class Solution29 {
+    /*
+
+    Trap for bad user input with the use of try and catch.
+
+    User will be stuck in a while loop until they enter the correct input.
+
+    The two exceptions that will be trapped are division by 0 and input mismatch.
+
+     */
+
+    // Demonstrates throwing an exception when a division by 0 occurs.
+    public static int InvestmentCalc(int years, int r)
+        throws ArithmeticException{
+        return years = 72 / r;
+    }
+
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        boolean continueLoop = true;
+
+    }
+
+}
+
