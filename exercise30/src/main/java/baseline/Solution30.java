7858f182b315aad7a162db6c9cf050cec0aa71c8
Tyler <tylerking885@knights.ucf.edu>
2021-10-02 13:29:36 -0400

    added code to exercise27

---
+++ exercise30/src/main/java/baseline/Solution30.java
@@ -0,0 +1,13 @@
+package baseline;
+
+public class Solution30 {
+    /*
+    Problem states to produce a multiplication table from 1 to 12 inclusive.
+
+    A 2d array will be used to store the multiplicative values.
+
+    A function will be used to multiply the indexes together and then stored into the 2d array.
+    The values will be stored using a nested for loop.
+
+     */
+}
