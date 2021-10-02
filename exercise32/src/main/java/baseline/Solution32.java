e0f20f0c969eac8e144d8d0b66f010609f9e1e08
Tyler <tylerking885@knights.ucf.edu>
2021-10-02 17:17:16 -0400

    added pseudocode to exercise32

---
+++ exercise32/src/main/java/baseline/Solution32.java
@@ -0,0 +1,18 @@
+package baseline;
+
+public class Solution32 {
+    // print out statement : Let's play Guess the Number!
+
+    // Prompt the user to enter a difficulty. (ranges from 1 to 3).
+
+    // If user chooses 1 generate a number from a set of values between 1 and 10.
+    // If user chooses 2 generate a number from a set of values between 1 and 100.
+    // If user chooses 3 generate a number from a set of values between 1 and 1000.
+
+    // Each time the user enters a value add to counter variable.
+    // Also display if the user is higher or lower than the random generated value.
+
+    // When the user enters the correct value display how many tries this took.
+
+    // Prompt the user if they wish to play again.
+}
