62aeb94b007ae39de91e7a91c612631e28eecd3b
Tyler <tylerking885@knights.ucf.edu>
2021-10-02 19:53:35 -0400

    added some pseudocode to exercise35

---
+++ exercise35/src/main/java/baseline/Solution35.java
@@ -0,0 +1,12 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solution33
+ *  Copyright 2021 Tyler King
+ */
+
+package baseline;
+
+public class Solution35 {
+    // Prompt for names of contestants until the user leaves the entry blank.
+    // Store the names in an ArrayList. Not including the blank entry.
+    // Create a random number generator that will pick a winner from the array List.
+}
