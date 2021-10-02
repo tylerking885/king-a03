504758107e2894757bbad2f901dbe6717e8f37cf
Tyler <tylerking885@knights.ucf.edu>
2021-10-02 17:03:59 -0400

    added code to exercise30

--- exercise30/src/main/java/baseline/Solution30.java
+++ exercise30/src/main/java/baseline/Solution30.java
@@ -1,13 +1,34 @@
 package baseline;
 
 public class Solution30 {
-    /*
-    Problem states to produce a multiplication table from 1 to 12 inclusive.
 
-    A 2d array will be used to store the multiplicative values.
+    public static void main(String[] args) {
 
-    A function will be used to multiply the indexes together and then stored into the 2d array.
-    The values will be stored using a nested for loop.
+        int [][] array;
 
-     */
+        array = multiplicationTable(12,12);
+
+        for (int[] ints : array) {
+            for (int anInt : ints) {
+                System.out.printf(" %3d ", anInt);
+            }
+            System.out.println();
+        }
+    }
+    // A function will be used to multiply the indexes together and then stored into the 2d array.
+    public static int[][] multiplicationTable(int R, int C){
+
+        int [][] array = new int[R][C];
+
+        // Use a nested loop to complete this program.
+        for (int row = 0; row < array.length; row++){
+
+            // Calculate the correct value at the correct index.
+            for (int column = 0; column < array[row].length; column++){
+                array[row][column] = (row + 1) * (column + 1);
+            }
+        }
+        // Returns the filled 2d array of multiplicative values.
+        return array;
+    }
 }
