e75a8910e76d7832ee962368b65b7f35873bd537
Tyler <tylerking885@knights.ucf.edu>
2021-10-03 12:40:58 -0400

    added pseudocode

---
+++ exercise37/src/main/java/baseline/Solution37.java
@@ -0,0 +1,16 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solution38
+ *  Copyright 2021 Tyler King
+ */
+
+package baseline;
+
+public class Solution37 {
+    // Store set of numbers in a string array list.
+    // Store set of special characters in a string array list.
+    // Store set of letters in a string array list.
+    // Prompt the user for a minimum password length.
+    // Prompt the user for number of special characters.
+    // Prompt the user for number of numbers.
+    // Generate a random password with at least as many letters are there are special characters and number.
+}
