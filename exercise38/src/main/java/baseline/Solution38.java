43fde82af632660fae7548607834e2c1eea14886
Tyler <tylerking885@knights.ucf.edu>
2021-10-02 20:28:01 -0400

    added pseudocode

---
+++ exercise38/src/main/java/baseline/Solution38.java
@@ -0,0 +1,15 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solution38
+ *  Copyright 2021 Tyler King
+ */
+
+package baseline;
+
+public class Solution38 {
+
+    // Prompt for a list of numbers, separated by spaces.
+    // Convert the input to an array.
+    // Have the program print out a new list containing only the even numbers.
+    // Use a function called filterEvenNumbers to encapsulate the logic for this.
+    // The function takes in the original array and returns the new array.
+}
